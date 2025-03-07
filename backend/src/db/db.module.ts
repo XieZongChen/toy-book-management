f27fbc966697b1385640b02b36037ea13873c505
XieZongChen <952937570@qq.com>
2025-03-07 14:53:01 +0800

    feat: db 模块使用动态模块

--- backend/src/db/db.module.ts
+++ backend/src/db/db.module.ts
@@ -1,7 +1,25 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { DbService } from './db.service';
 
-@Module({
-  providers: [DbService]
-})
-export class DbModule {}
+export interface DbModuleOptions {
+  path: string;
+}
+
+@Module({})
+export class DbModule {
+  // 动态模块
+  static register(options: DbModuleOptions): DynamicModule {
+    // 根据 register 的参数来放回不同的模块配置
+    return {
+      module: DbModule,
+      providers: [
+        {
+          provide: 'OPTIONS',
+          useValue: options,
+        },
+        DbService,
+      ],
+      exports: [DbService],
+    };
+  }
+}
