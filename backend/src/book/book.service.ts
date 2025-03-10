01efe6dc9119a1d8bcb874a59d4f8ebb485f55e9
XieZongChen <952937570@qq.com>
2025-03-10 09:58:54 +0800

    feat: 规划 book service

--- backend/src/book/book.service.ts
+++ backend/src/book/book.service.ts
@@ -1,4 +1,16 @@
+import { UpdateBookDto } from './dto/update-book.dto';
+import { CreateBookDto } from './dto/create-book.dto';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  list() {}
+
+  findById(id: number) {}
+
+  create(createBookDto: CreateBookDto) {}
+
+  update(updateBookDto: UpdateBookDto) {}
+
+  delete(id: number) {}
+}
