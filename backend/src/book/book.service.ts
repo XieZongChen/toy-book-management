4839e82cfae2ae32e9ab0951daa472e8306fa03d
XieZongChen <952937570@qq.com>
2025-03-10 10:05:11 +0800

    feat: 实现 book service

--- backend/src/book/book.service.ts
+++ backend/src/book/book.service.ts
@@ -1,16 +1,69 @@
 import { UpdateBookDto } from './dto/update-book.dto';
 import { CreateBookDto } from './dto/create-book.dto';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { DbService } from 'src/db/db.service';
+import { Book } from './entities/book.entity';
+
+function randomNum() {
+  return Math.floor(Math.random() * 1000000);
+}
 
 @Injectable()
 export class BookService {
-  list() {}
+  @Inject()
+  dbService: DbService;
 
-  findById(id: number) {}
+  async list() {
+    const books: Book[] = await (this.dbService.read() as Promise<Book[]>);
+    return books;
+  }
 
-  create(createBookDto: CreateBookDto) {}
+  async findById(id: number) {
+    const books: Book[] = await (this.dbService.read() as Promise<Book[]>);
+    return books.find((book) => book.id === id);
+  }
 
-  update(updateBookDto: UpdateBookDto) {}
+  async create(createBookDto: CreateBookDto) {
+    const books: Book[] = await (this.dbService.read() as Promise<Book[]>);
 
-  delete(id: number) {}
+    const book = new Book();
+    book.id = randomNum();
+    book.author = createBookDto.author;
+    book.name = createBookDto.name;
+    book.description = createBookDto.description;
+    book.cover = createBookDto.cover;
+
+    books.push(book);
+
+    await this.dbService.write(books);
+    return book;
+  }
+
+  async update(updateBookDto: UpdateBookDto) {
+    const books: Book[] = await (this.dbService.read() as Promise<Book[]>);
+
+    const foundBook = books.find((book) => book.id === updateBookDto.id);
+
+    if (!foundBook) {
+      throw new BadRequestException('该图书不存在');
+    }
+
+    foundBook.author = updateBookDto.author;
+    foundBook.cover = updateBookDto.cover;
+    foundBook.description = updateBookDto.description;
+    foundBook.name = updateBookDto.name;
+
+    await this.dbService.write(books);
+    return foundBook;
+  }
+
+  async delete(id: number) {
+    const books: Book[] = await (this.dbService.read() as Promise<Book[]>);
+    const index = books.findIndex((book) => book.id === id);
+
+    if (index !== -1) {
+      books.splice(index, 1);
+      await this.dbService.write(books);
+    }
+  }
 }
