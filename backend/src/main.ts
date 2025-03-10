fdca29a1fdb9f734160bb62fd47f1f729903f1d8
XieZongChen <952937570@qq.com>
2025-03-10 11:46:47 +0800

    feat: 上传测试 & 静态资源访问设置

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // 校验请求体的参数
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  // 访问上传的静态资源的设置
+  app.useStaticAssets(join(__dirname, '../uploads'), { prefix: '/uploads' });
 
   await app.listen(process.env.PORT ?? 3000);
 }
