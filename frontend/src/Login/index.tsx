1c87c0566d3ff41e19668b3adbaa513749b71a3e
XieZongChen <952937570@qq.com>
2025-03-11 11:16:08 +0800

    feat: 优化 login 样式

--- frontend/src/Login/index.tsx
+++ frontend/src/Login/index.tsx
@@ -36,45 +36,46 @@ export function Login() {
   return (
     <div
       id='login-container'
-      className='h-[100vh] flex flex-col justify-center items-center gap-[30px]'
+      className='h-[100vh] flex flex-col justify-center items-center'
     >
-      <h1 className='text-4xl font-bold tracking-wider'>图书管理系统</h1>
-
-      <Form
-        {...layout1}
-        onFinish={onFinish}
-        colon={false}
-        autoComplete='off'
-        className='w-[400px]'
-      >
-        <Form.Item
-          label='用户名'
-          name='username'
-          rules={[{ required: true, message: '请输入用户名!' }]}
+      <div className='w-[500px] px-[50px] py-[80px] border border-gray-200 rounded-2xl flex flex-col items-center gap-[60px]'>
+        <h1 className='text-4xl font-bold tracking-wider'>图书管理系统</h1>
+        <Form
+          {...layout1}
+          onFinish={onFinish}
+          colon={false}
+          autoComplete='off'
+          className='w-full'
         >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            label='用户名'
+            name='username'
+            rules={[{ required: true, message: '请输入用户名!' }]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item
-          label='密码'
-          name='password'
-          rules={[{ required: true, message: '请输入密码!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Form.Item
+            label='密码'
+            name='password'
+            rules={[{ required: true, message: '请输入密码!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item {...layout2}>
-          <div className='flex justify-center'>
-            <a href='/register'>没有账号？去注册</a>
-          </div>
-        </Form.Item>
+          <Form.Item {...layout2}>
+            <div className='flex justify-center'>
+              <a href='/register'>没有账号？去注册</a>
+            </div>
+          </Form.Item>
 
-        <Form.Item {...layout2} className='flex justify-center'>
-          <Button className='w-full' type='primary' htmlType='submit'>
-            登录
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item {...layout2} className='flex justify-center'>
+            <Button className='w-full' type='primary' htmlType='submit'>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 }
