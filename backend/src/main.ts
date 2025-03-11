c1a1edcd33e965dccf082e75202f255408471433
XieZongChen <952937570@qq.com>
2025-03-11 10:00:56 +0800

    feat: 后端支持接口跨域

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,9 @@ async function bootstrap() {
   // 访问上传的静态资源的设置
   app.useStaticAssets(join(__dirname, '../uploads'), { prefix: '/uploads' });
 
+  // 支持跨域访问
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
